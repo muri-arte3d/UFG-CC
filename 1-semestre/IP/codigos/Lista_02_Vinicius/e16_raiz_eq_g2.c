295c40248d322f77acb2b6bb5a0a5f681f8693a5
muri-arte3d <muriloarte3d@gmail.com>
2024-10-08 18:29:40 -0300

    Atualiza 08-10-2024

--- 1-semestre/IP/codigos/Lista_02_Vinicius/e16_raiz_eq_g2.c
+++ 1-semestre/IP/codigos/Lista_02_Vinicius/e16_raiz_eq_g2.c
@@ -4,10 +4,10 @@
 
 int main()
 {
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
 
-    float delta = b * b - 4 * a * c;
+    double delta = b * b - 4 * a * c;
 
     if (delta < 0)
     {
@@ -15,24 +15,24 @@ int main()
     }
     else if (delta == 0)
     {
-        float x1 = (-b - sqrt(delta)) / (2.0 * a);
+        double x1 = (-b) / (2.0 * a);
         printf("RAIZ UNICA\n");
-        printf("X1 = %.2f\n", x1);
+        printf("X1 = %.2lf\n", x1);
     }
     else
     {
-        float x1 = (-b - sqrt(delta)) / (2.0 * a);
-        float x2 = (-b + sqrt(delta)) / (2.0 * a);
+        double x1 = (-b - sqrt(delta)) / (2.0 * a);
+        double x2 = (-b + sqrt(delta)) / (2.0 * a);
         if (x1 > x2)
         {
-            int temp = x1;
+            double temp = x1;
             x1 = x2;
             x2 = temp;
         }
 
         printf("RAIZES DISTINTAS\n");
-        printf("X1 = %.2f\n", x1);
-        printf("X2 = %.2f\n", x2);
+        printf("X1 = %.2lf\n", x1);
+        printf("X2 = %.2lf\n", x2);
     }
 
     return 0;
--- 1-semestre/IP/codigos/Lista_02_Vinicius/e23_lucro_merc.c
+++ 1-semestre/IP/codigos/Lista_02_Vinicius/e23_lucro_merc.c
@@ -4,21 +4,65 @@
 
 int main()
 {
+    int count = 0;
     unsigned long int cm; // codigo de mercadoria
-    float pcompra;        // preço de compra da mercadoria
-    float pvenda;         // preço de venda da mercadoria
+    double pcompra;       // preço de compra da mercadoria
+    double pvenda;        // preço de venda da mercadoria
     int nvenda;           // número de venda da mercadoria
-    scanf("%d", &cm);
-    printf("%d\n", '\n');
 
-    while (1)
+    int l1 = 0, l2 = 0, l3 = 0, cmaiorl = 0, cmvend = 0;
+    double lucro, maiorlucro = 0, ctotal = 0, vtotal = 0, ltotal = 0;
+    int maisvendida = 0;
+
+    while (scanf("%d ", &cm) != EOF)
     {
-        scanf("%d ", &cm);
-        if(cm == '\n'){
-            break;
-        }
-        scanf("%f %f %f", &pcompra, &pvenda, &nvenda);
-    }
+        //scanf("%d ", &cm);
+        scanf("%lf %lf %d", &pcompra, &pvenda, &nvenda);
+        //printf("numero de vendas: %d\n", nvenda);
 
+        // Classifica mercadoria por lucro
+        lucro = (pvenda - pcompra) / pcompra;
+        if (lucro < 0.1)
+        {
+            l1++;
+        }
+        else if (lucro <= 0.2)
+        {
+            l2++;
+        }
+        else
+        {
+            l3++;
+        }
+        // Código de mercadoria de maior lucro
+        if (lucro > maiorlucro)
+        {
+            cmaiorl = cm;
+            maiorlucro = lucro;
+        }
+        // Código de mercadoria mais vendida
+        if (nvenda > maisvendida)
+        {
+            cmvend = cm;
+            maisvendida = nvenda;
+            //printf("A mais vendida: %d\n", maisvendida);
+        }
+        ctotal += nvenda * pcompra;
+        vtotal += nvenda * pvenda;
+        //ltotal += lucro;
+        //count++;
+        //printf("lucro total: %.2lf\n", ltotal);
+
+       // printf("flag: ");
+       // scanf("%d", &flag);
+    }
+    ltotal = (vtotal - ctotal) * 100 / ctotal;
+
+    printf("Quantidade de mercadorias que geraram lucro menor que 10%%: %d\n", l1);
+    printf("Quantidade de mercadorias que geraram lucro maior ou igual a 10%% e menor ou igual a 20%%: %d\n", l2);
+    printf("Quantidade de mercadorias que geraram lucro maior do que 20%%: %d\n", l3);
+    printf("Codigo da mercadoria que gerou maior lucro: %d\n", cmaiorl);
+    printf("Codigo da mercadoria mais vendida: %d\n", cmvend);
+    printf("Valor total de compras: %.2lf, valor total de vendas: %.2lf e percentual de lucro total: %.2lf%%\n", ctotal, vtotal, ltotal);
     return 0;
 }
